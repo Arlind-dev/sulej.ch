ded6f55bd841c40f72652fb9a6ba897329e15c57
Arlind-dev <sulejmaniarlind5@gmail.com>
2024-05-19 19:44:44 +0200

    change navbar colour

--- src/components/molecules/NavHost.svelte
+++ src/components/molecules/NavHost.svelte
@@ -1,48 +1,24 @@
 <script lang="ts">
 	import Nav from '../atoms/Nav.svelte';
 	let y: number;
+	import { HomeSimpleIcon } from '@indaco/svelte-iconoir/home-simple';
+	import { InfoCircleIcon } from '@indaco/svelte-iconoir/info-circle';
+	import { BagIcon } from '@indaco/svelte-iconoir/bag';
 </script>
 
 <nav>
 	<div class:scrolled={y > 20}>
-		<ul>
-			<Nav href="#home" section="/" isSelected={y < 100}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					fill="var(--accent)"
-					viewBox="0 0 24 24"
-				><path fill="none" d="M0 0h24v24H0z" /><path
-					d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"
-				/></svg
-				>
-			</Nav>
-			<Nav href="#about" section="about" isSelected={y >= 100 && y <= 400}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					fill="var(--accent)"
-					viewBox="0 0 24 24"
-				><path fill="none" d="M0 0h24v24H0z" /><path
-					d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"
-				/></svg
-				>
-			</Nav>
-			<Nav href="#work" section="work" isSelected={y > 400}>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="24"
-					height="24"
-					fill="var(--accent)"
-					viewBox="0 0 24 24"
-				><path fill="none" d="M0 0h24v24H0z" /><path
-					d="M20 6h-4V4c0-1.11-.89-2-2-2h-4c-1.11 0-2 .89-2 2v2H4c-1.11 0-1.99.89-1.99 2L2 19c0 1.11.89 2 2 2h16c1.11 0 2-.89 2-2V8c0-1.11-.89-2-2-2zm-6 0h-4V4h4v2z"
-				/></svg
-				>
-			</Nav>
-		</ul>
+			<ul>
+					<Nav href="#home" section="/" isSelected={y < 100}>
+							<HomeSimpleIcon width="24" height="24" fill="var(--accent)" />
+					</Nav>
+					<Nav href="#about" section="about" isSelected={y >= 100 && y <= 300}>
+							<InfoCircleIcon width="24" height="24" fill="var(--accent)" />
+					</Nav>
+					<Nav href="#work" section="work" isSelected={y > 300}>
+							<BagIcon width="24" height="24" fill="var(--accent)" />
+					</Nav>
+			</ul>
 	</div>
 </nav>
 
@@ -94,7 +70,7 @@
 			bottom: 0;
 			top: auto;
 			padding: 0rem;
-			background-color: var(--elevation-one);
+			background-color: var(--navbar);
 		}
 
 		div ul {
