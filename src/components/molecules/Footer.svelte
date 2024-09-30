9a521f52551c66b56d1ae4f616ac91ca14a57579
Arlind <arlind@sulej.ch>
2024-09-30 13:02:31 +0200

    v0.5.4

--- src/components/molecules/Footer.svelte
+++ src/components/molecules/Footer.svelte
@@ -11,7 +11,7 @@
       ><a
         href="https://github.com/Arlind-dev/sulej.ch"
         target="_blank"
-        rel="noreferrer">v0.5.3</a
+        rel="noreferrer">v0.5.4</a
       ></Tooltip
     >
   </h6>
