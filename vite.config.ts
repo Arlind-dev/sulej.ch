4c25a944ab457299bd8ca7e680729854cf2598a9
Arlind-dev <arlind@sulej.ch>
2024-09-30 15:38:46 +0200

    revert

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,4 @@ export default defineConfig({
       }
     }
   },
-  start: {
-    server: {
-      preset: 'cloudflare_module',
-      rollupConfig: {
-        external: ['__STATIC_CONTENT_MANIFEST', 'node:async_hooks'],
-      },
-    },
-  },
 });
